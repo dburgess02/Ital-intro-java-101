a3ec7d636101a567e5b0df05c2e7f9476f0fc386
dburgess02 <burgessdwayne2002@gmail.com>
2025-06-21 16:30:23 -0500

    Create Guess.game.java

---
+++ Guess.game.java
@@ -0,0 +1,15 @@
+//Print out welcom screen 
+To print out a "Welcome screen" we would do a "system.out" command.
+
+//instructions
+A "system.out" command, saying "please guess a number between 1-23"
+ 
+ //Show if the user was correct or not
+ Now that we have displayed the welcome screen, and a set of instrucctions. We will now let the user know if their attempts was correct or not.
+ In this situation we would use "if else" statement. If the conditions are true based on what the statement that has been typed are true, it should display a correct or incorrect as a message.
+ 
+// Giving the user more than one try: 
+Giving the user the ability to run the game more than once would require a for loop. 
+
+//end        
+	 
